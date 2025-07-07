a0a30790b2d713cee89f31d613a2f208325a53ff
Daniellagotthesauce <daniella.wambua@strathmore.edu>
2025-07-07 17:26:13 +0300

    Working Feedback Form

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 // This file will handle the main application logic
 import express from 'express'; // for creating the server
 import cors from 'cors'; // for handling CORS
+import bodyParser from 'body-parser';
 import otpRoutes from './routes/otpRoutes.js'; // for registration and OTP routes
 import authRoutes from './routes/authRoutes.js'; // for login authentication route
 import menuRoutes from './routes/menuRoutes.js'; // for menu routes
@@ -10,12 +11,14 @@ import resetPasswordRoutes from './routes/resetPasswordRoutes.js'; // for passwo
 import adminRoutes from './routes/adminRoutes.js'; // for admin routes
 import staffRoutes from './routes/staffRoutes.js' // Staff Routes
 import { stkPush } from './mpesa/mpesa.js';
+import feedbackRoutes from './routes/feedbackRoutes.js'
 
 const app = express();
 
 // ✅ FIRST: Middleware
 app.use(cors());
 app.use(express.json()); // Must come BEFORE routes
+app.use(bodyParser.json())
 
 // ✅ STK Push route
 const mpesaRouter = express.Router();
@@ -42,5 +45,6 @@ app.use('/api/admin', adminRoutes);
 app.use('/api/admin', deliveryLocationRoutes);
 
 app.use('/api/staff', staffRoutes);
+app.use('/api', feedbackRoutes);
 
 export default app;
---
+++ backend/models/Feedback.js
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose';
+
+const feedbackSchema = new mongoose.Schema({
+    name : { type: String, required:true, trim: true},
+    email: { type:String, required: true, lowercase: true},
+    message: {type: String, required: true},
+}, { timestamps: true});
+
+export default mongoose.model("Feedback", feedbackSchema) ;
